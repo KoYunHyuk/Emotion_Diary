dd15810c4f54d1bfcdc8efc8298fa4070a40faea
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2022-03-25 01:48:53 +0900

    Complete 'Diary.js' Page

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -7,49 +7,8 @@ import EmotionItem from './EmotionItem'
 import MyButton from './MyButton'
 import MyHeader from './MyHeader'
 
-const emotionList = [
-    {
-        emotion_id: 1,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-        emotion_descript: "완전 좋음",
-    },
-    {
-        emotion_id: 2,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-        emotion_descript: "좋음",
-    },
-    {
-        emotion_id: 3,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-        emotion_descript: "그럭저럭",
-    },
-    {
-        emotion_id: 4,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-        emotion_descript: "나쁨",
-    },
-    {
-        emotion_id: 5,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-        emotion_descript: "끔직함",
-    },
-]
-
-const getStringDate = (date) => {
-    // return date.toISOString().slice(0, 10);
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-
-    if(month < 10) {
-        month = `0${month}`;
-    }
-    if(day < 10) {
-        day = `0${day}`;
-    }
-    
-    return `${year}-${month}-${day}`;
-}
+import { getStringDate } from '../util/date.js'
+import { emotionList } from '../util/emotions'
 
 function DiaryEditor({isEdit, originData}) {
     const navigate = useNavigate();
